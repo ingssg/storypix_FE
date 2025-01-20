e3a904378ce699f52b81f8f95979b2fab76e36ed
Inseok Kim <js2121good@gmail.com>
2025-01-20 18:51:44 +0900

    Add: 더미 데이터 추가

---
+++ src/app/list/page.tsx
@@ -0,0 +1,22 @@
+const dummy = {
+  titleKor: "양치기 소년",
+  titleEng: "The Boy Who Cried Wolf",
+  description:
+    "거짓말을 하던 소년이 진짜 위험에 처했을 때 아무도 믿어주지 않은 이야기",
+  image: "/images/dummy-tale.svg",
+  minuteLength: 5,
+  totalPage: 3,
+  price: 0,
+  isAccessible: true,
+};
+
+const dummy1 = {
+  titleKor: "백설공주",
+  titleEng: "Snow White",
+  description: "백설공주가 사과를 먹고 잠들어버린 이야기",
+  image: "/images/dummy-tale.svg",
+  minuteLength: 10,
+  totalPage: 3,
+  price: 9900,
+  isAccessible: false,
+};

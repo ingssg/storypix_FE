68b579152502ed8ab521c9862143f99ce8ae55bd
Inseok Kim <js2121good@gmail.com>
2025-01-22 16:31:13 +0900

    Add: 더미 파일 추가

---
+++ src/components/playerHover/dummy.ts
@@ -0,0 +1,50 @@
+import { StoryContents } from "@/app/store/playerStore";
+
+export const dummy: StoryContents = [
+  {
+    page: 1,
+    details: [
+      {
+        text: "In the middle of the forest, a big lion was sleeping under a tree. His tummy went up and down as he snored. The forest was very quiet, and soft sunlight came through the leaves.",
+        narration: "/images/dummy/1-1.mp3",
+      },
+    ],
+    image: "/images/dummy/Scene1.png",
+  },
+  {
+    page: 2,
+    details: [
+      {
+        text: "The lion’s big eyes opened, and he roared very loudly. “Who woke me up?!” he shouted, his voice rumbling like thunder.",
+        narration: "/images/dummy/3-1.mp3",
+      },
+      {
+        text: "The mouse froze. Its little body shook, and it squeaked, “Please don’t eat me! I’m just a tiny mouse. I didn’t mean to wake you up!”",
+        narration: "/images/dummy/3-2.mp3",
+      },
+      {
+        text: "The lion stared at the mouse with his sharp teeth showing. “You woke me up! I should eat you!”",
+        narration: "/images/dummy/3-3.mp3",
+      },
+      {
+        text: "The mouse begged, “Please don’t eat me! I’m so small, but I promise I will help you one day!”",
+        narration: "/images/dummy/3-4.mp3",
+      },
+      {
+        text: "The lion laughed. “You? Help me? That’s so funny!” But after thinking for a moment, the lion stopped growling. “You’re too small to bother with,” he said, and he let the mouse go.",
+        narration: "/images/dummy/3-5.mp3",
+      },
+    ],
+    image: "/images/dummy/Scene3.png",
+  },
+  {
+    page: 3,
+    details: [
+      {
+        text: "The little mouse was so scared, it ran as fast as it could! Its tiny legs went pitter-patter, and its long tail swung behind it. The mouse ran into the grass and didn’t stop until it felt safe again.",
+        narration: "/images/dummy/4-1.mp3",
+      },
+    ],
+    image: "/images/dummy/Scene4.png",
+  },
+];

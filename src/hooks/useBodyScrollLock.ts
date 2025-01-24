33857908a48e378df18464a3ddf9e5db071c4580
Inseok Kim <js2121good@gmail.com>
2025-01-24 10:31:47 +0900

    Fix: document 예외처리 추가

--- src/hooks/useBodyScrollLock.ts
+++ src/hooks/useBodyScrollLock.ts
@@ -4,14 +4,16 @@ import { useEffect } from "react";
 
 export const useBodyScrollLock = (isOpenModal: boolean) => {
   useEffect(() => {
-    if (isOpenModal) {
-      document.body.style.overflow = "hidden";
-      return;
-    }
-    document.body.style.overflow = "";
-
-    return () => {
+    if (typeof document !== "undefined") {
+      if (isOpenModal) {
+        document.body.style.overflow = "hidden";
+        return;
+      }
       document.body.style.overflow = "";
-    };
+  
+      return () => {
+        document.body.style.overflow = "";
+      };
+    }
   }, [isOpenModal]);
 };

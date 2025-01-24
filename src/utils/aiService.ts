54620c8095768af8094b03f395cd82cdf8e3f910
Inseok Kim <js2121good@gmail.com>
2025-01-24 09:45:18 +0900

    Feat: openai 임시토큰 발급

---
+++ src/utils/aiService.ts
@@ -0,0 +1,18 @@
+export const getToken = async () => {
+  const token = await fetch("https://api.openai.com/v1/realtime/sessions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-mini-realtime-preview-2024-12-17",
+      voice: "verse",
+      modalities: ["text", "audio"],
+    }),
+  });
+  const data = await token.json();
+
+  return data;
+};
+

c572c8daeb4a19a98a5c6408e26a552fbfb8e8f8
Inseok Kim <js2121good@gmail.com>
2025-01-24 09:45:50 +0900

    Feat: 가로모드 권장 모달 추가

---
+++ src/components/viewOptimizationModal.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+
+const ViewOptimizationModal = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+    className={`fixed top-10 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-gray-500 text-white rounded-lg transition-opacity duration-1000 w-56 text-center ${
+      isVisible ? "opacity-100" : "opacity-0"
+    }`}
+  >
+    가로 모드 사용을 권장합니다
+  </div>
+  )
+}
+
+export default ViewOptimizationModal

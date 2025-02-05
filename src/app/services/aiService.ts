eecdf12b5d42c7c8876a0ffa7f02bd4a043920b9
Inseok Kim <js2121good@gmail.com>
2025-02-05 10:26:52 +0900

    Feat: 소통기록 전송 api

--- src/app/services/aiService.ts
+++ src/app/services/aiService.ts
@@ -1,6 +1,29 @@
+import { apiClient } from "../lib/apiClient";
+
+export interface Communication {
+  storyId: number;
+  openaiSessionId: string;
+  questionPage: number;
+  previousSentence: string;
+  currentSentence: string;
+  records: Record[];
+  createdAt: Date;
+}
+
+export type Record = {
+  text: string;
+  isUser: boolean;
+  createdAt: Date;
+};
+
 export const getTokenAPI = async (storyId: number) => {
   const { data } = await apiClient.post("/communications/session", {
     storyId,
   });
+  return data;
+};
+
+export const postCommuicationAPI = async (communication: Communication) => {
+  const { data } = await apiClient.post("/communications", communication);
   return data;
 };

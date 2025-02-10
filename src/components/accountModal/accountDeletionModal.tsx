c813cd7b46341f2b0a194058aab804a5a112cbfe
Inseok Kim <js2121good@gmail.com>
2025-02-10 11:27:23 +0900

    Remove: router 삭제

--- src/components/accountModal/accountDeletionModal.tsx
+++ src/components/accountModal/accountDeletionModal.tsx
@@ -1,6 +1,5 @@
 import { deleteUser } from "@/app/services/userService";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 type Props = {
@@ -8,7 +7,6 @@ type Props = {
 };
 
 const AccountDeletionModal = ({ onClose }: Props) => {
-  const router = useRouter();
 
   const modalClose = () => onClose();
   const [inputValue, setInputValue] = useState("");
--- src/components/accountModal/logoutModal.tsx
+++ src/components/accountModal/logoutModal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { signout } from "@/app/services/userService";
 
 type Props = {
@@ -8,7 +7,6 @@ type Props = {
 };
 
 const LogOutModal = ({ onClose }: Props) => {
-  const router = useRouter();
 
   const modalClose = () => onClose();
 

f78a9ddfbfffe58034fe0e1e27dc9e7d0ef7a58e
Inseok Kim <js2121good@gmail.com>
2025-02-10 17:10:40 +0900

    Remove: 주석 제거

--- src/components/playerHover/settingModal.tsx
+++ src/components/playerHover/settingModal.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, { useEffect } from "react";
 import { usePlayerStore } from "@/app/store/playerStore";
@@ -10,14 +10,21 @@ type Props = {
 };
 
 const SettingModal = ({ onClose }: Props) => {
-  const { decreaseSpeed, resetSpeed, increaseSpeed, setKorean, setEnglish, speed, language } =
-    usePlayerStore();
+  const {
+    decreaseSpeed,
+    resetSpeed,
+    increaseSpeed,
+    setKorean,
+    setEnglish,
+    speed,
+    language,
+  } = usePlayerStore();
 
-    const slowButton = useRef<HTMLButtonElement>(null);
-    const normalButton = useRef<HTMLButtonElement>(null);
-    const fastButton = useRef<HTMLButtonElement>(null);
-    const koreanButton = useRef<HTMLButtonElement>(null);
-    const englishButton = useRef<HTMLButtonElement>(null);
+  const slowButton = useRef<HTMLButtonElement>(null);
+  const normalButton = useRef<HTMLButtonElement>(null);
+  const fastButton = useRef<HTMLButtonElement>(null);
+  const koreanButton = useRef<HTMLButtonElement>(null);
+  const englishButton = useRef<HTMLButtonElement>(null);
 
   const closeSettings = (e: React.MouseEvent<HTMLButtonElement>) => {
     onClose();
@@ -30,7 +37,7 @@ const SettingModal = ({ onClose }: Props) => {
     normalButton.current?.classList.remove("selected");
     fastButton.current?.classList.remove("selected");
     e.stopPropagation();
-  }
+  };
 
   const resetSpeedBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
     resetSpeed();
@@ -38,7 +45,7 @@ const SettingModal = ({ onClose }: Props) => {
     normalButton.current?.classList.add("selected");
     fastButton.current?.classList.remove("selected");
     e.stopPropagation();
-  }
+  };
 
   const increaseSpeedBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
     increaseSpeed();
@@ -46,25 +53,24 @@ const SettingModal = ({ onClose }: Props) => {
     normalButton.current?.classList.remove("selected");
     fastButton.current?.classList.add("selected");
     e.stopPropagation();
-  }
+  };
 
   const setKoreanBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
     setKorean();
     koreanButton.current?.classList.add("selected");
     englishButton.current?.classList.remove("selected");
     e.stopPropagation();
-  }
+  };
 
   const setEnglishBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
     setEnglish();
     koreanButton.current?.classList.remove("selected");
     englishButton.current?.classList.add("selected");
     e.stopPropagation();
-  }
-
+  };
 
   useEffect(() => {
-    switch(speed) {
+    switch (speed) {
       case 0.8:
         slowButton.current?.classList.add("selected");
         break;
@@ -75,7 +81,7 @@ const SettingModal = ({ onClose }: Props) => {
         fastButton.current?.classList.add("selected");
         break;
     }
-    switch(language) {
+    switch (language) {
       case "korean":
         koreanButton.current?.classList.add("selected");
         break;
@@ -83,13 +89,12 @@ const SettingModal = ({ onClose }: Props) => {
         englishButton.current?.classList.add("selected");
         break;
     }
-  }, [])
+  }, []);
 
   return (
     <div
       className=" h-full absolute right-0 z-[21]"
-      onClick={(e:React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
-      // ref={settingModal}
+      onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
     >
       <div className="flex flex-col justify-center items-center bg-white bg-opacity-60 backdrop-blur-lg h-[95%] rounded-xl mr-4 mt-[3%] p-4 w-68">
         <div className="flex justify-between items-center w-full text-black">

475f62eed29b856e25ae95ae5a1a9033dfd69e74
Inseok Kim <js2121good@gmail.com>
2025-02-14 16:04:27 +0900

    Feat: Gtag 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
+import GtagWrapper from "@/components/gtagWrapper";
+import { Suspense } from "react";
 
 const pretendard = localFont({
   src: "../fonts/PretendardVariable.woff2",
@@ -19,8 +22,8 @@ export const metadata: Metadata = {
   title: "StoryPix",
   description: "for Children",
   other: {
-    'mobile-web-app-capable': 'yes',
-    'apple-mobile-web-app-capable': 'yes',
+    "mobile-web-app-capable": "yes",
+    "apple-mobile-web-app-capable": "yes",
   },
   icons: {
     icon: "/favicon.ico",
@@ -39,7 +42,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${pretendard.className} ${hammersmith.className} font-pretendard`}>{children}</body>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CMDTLDZ8M4"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-CMDTLDZ8M4');
+          `}
+        </Script>
+      </head>
+      <body
+        className={`${pretendard.className} ${hammersmith.className} font-pretendard`}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <GtagWrapper />
+        </Suspense>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/gtagWrapper.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import useGtag from "@/hooks/useGtag";
+
+const GtagWrapper = () => {
+  useGtag();
+  return null;
+};
+
+export default GtagWrapper;

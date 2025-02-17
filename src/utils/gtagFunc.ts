385e08a8d69251a6d7d40d98551419f8330b0e7f
Inseok Kim <js2121good@gmail.com>
2025-02-17 10:34:38 +0900

    Feat: gtagFunc 추가

--- src/utils/gtagFunc.ts
+++ src/utils/gtagFunc.ts
@@ -1,7 +1,19 @@
-const trackingEvent = (event: string, params: Record<string, unknown>) => {
+import { usePlayerStore } from "@/app/store/playerStore";
+import { getNickName } from "./stores";
+
+export const trackingEvent = (event: string, params: Record<string, unknown> = {}) => {
   if (window.gtag) {
     window.gtag("event", event, params);
+    // console.log("trackingEvent", event, params);
   }
 };
 
-export default trackingEvent;
+export const trackingPlayerEvent = (event: string, params: Record<string, unknown> = {}) => {
+  const { storyId } = usePlayerStore.getState();
+  if (window.gtag) {
+    params.story_id = storyId;
+    params.user_id = getNickName();
+    params.timestamp = Date.now();
+    window.gtag("event", event, params);
+  }
+}

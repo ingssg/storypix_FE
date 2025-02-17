f7a16b4ac6aadeb2d4026df4f343917ce1431ba8
Inseok Kim <js2121good@gmail.com>
2025-02-17 18:24:04 +0900

    Add: 미들웨어 디버깅

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
+  console.log("middleware");
+  console.log(req.cookies);
   const url = req.nextUrl.clone();
 
   if (url.pathname === "/") {

4b146d2251af85bd600c0aca87783c3bf1b27782
Inseok Kim <js2121good@gmail.com>
2025-02-20 15:59:30 +0900

    Refactor: 질문취소 컴포넌트화

---
+++ src/components/aiModalComponents/cancelQuestion.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CancelQuestion() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <p className="text-[#46474C] font-semibold text-sm text-center">
+        버튼을 눌러 동화 내용이나
+        <br />
+        영어 표현에 대해 질문해보세요.
+      </p>
+    </div>
+  );
+}
+
+export default CancelQuestion;

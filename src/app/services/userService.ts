14a276f57c24a3446a6d1e3ff4e620e179b1614c
Inseok Kim <js2121good@gmail.com>
2025-02-21 14:55:35 +0900

    Feat: 로그인시 gnb 닉네임 추가

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -1,11 +1,14 @@
 import { apiClient } from "../lib/apiClient";
+import { useUserStore } from "../store/userStore";
 
 // 유저 관련 api 집합
 
 export const fetchUser = async () => {
+  const { setHasLogin } = useUserStore.getState();
   try {
     const { data } = await apiClient.get("/users/me");
     localStorage.setItem("nickname", data.userInfo.nickname);
+    setHasLogin(true);
     return data;
   } catch (error) {
     console.error(error);
